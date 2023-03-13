724d1adc2aed02828c3258d7bb763ebd289bebd8
John Danielsson <JohnDanielsson@JDs-MacBook-Pro-work.local>
2023-03-13 10:20:04 -0700

    updated chart properties

--- app/graph.py
+++ app/graph.py
@@ -3,9 +3,16 @@ from pandas import DataFrame
 
 
 def chart(df: DataFrame, x: str, y: str, target: str) -> Chart:
-    return Chart(
+    chart = Chart(
         data=df,
         mark='line',
         description='Data visualization of Bandersnatch data with an Altair Chart',
         title=f'x={x}, y={y}, target={target}'
     )
+    chart.properties(
+        width=200,
+        height=200,
+        background='ffffff',
+        padding=10
+    )
+    return chart
