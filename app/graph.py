569a21fd787c534decb44f4a4c7abde355fcb477
John Danielsson <JohnDanielsson@JDs-MacBook-Pro-work.local>
2023-03-13 10:13:36 -0700

    func def done

--- app/graph.py
+++ app/graph.py
@@ -1,5 +1,11 @@
 from altair import Chart
+from pandas import DataFrame
 
 
-def chart(df, x, y, target) -> Chart:
-    pass
+def chart(df: DataFrame, x: str, y: str, target: str) -> Chart:
+    return Chart(
+        data=df,
+        mark='line',
+        description='Data visualization of Bandersnatch data with an Altair Chart',
+        title=f'x={x}, y={y}, target={target}'
+    )
