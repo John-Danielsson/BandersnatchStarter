53a1099ed76e5899b40169dc3e8d21661ac6bbc7
John Danielsson <JohnDanielsson@JDs-MacBook-Pro-work.local>
2023-03-15 06:59:21 -0700

    to do + pep8

--- app/machine.py
+++ app/machine.py
@@ -1,8 +1,12 @@
 from pandas import DataFrame
 from sklearn.ensemble import RandomForestClassifier
 from datetime import datetime
+from joblib import load, dump
+
 
 class Machine:
+
+    # TO DO: download data as a .csv, use in colab for testing
 
     # Does the class properly handle and store the target
     # and feature data when initializing the model?
@@ -12,33 +16,35 @@ class Machine:
     def __init__(self, df: DataFrame):
         self.name = "Random Forest Classifier"
         target = df['Rarity']
-        X = df.drop(columns=['Rarity'])
+        features = df.drop(columns=['Rarity'])
         self.model = RandomForestClassifier()
-        self.model.fit(X, target)
+        self.model.fit(features, target)
         self.timestamp = '%s' % datetime.now()
 
     # Does the call function take in a DataFrame of feature data and
     # return a prediction and the probability of the prediction?
     def __call__(self, feature_basis: DataFrame):
         prediction, *_ = self.model.predict(feature_basis)
+        print('prediction', type(prediction))
         return prediction
 
     # Does `save()` properly save the machine learning model
     # to the specified filepath using joblib?
-    def save(self, filepath):
-        pass
+    def save(self, filepath: str) -> None:
+        dump(value=self.model, filename=filepath)
 
     # Does `open()` properly load a saved machine learning model
     # from the specified filepath using joblib?
     @staticmethod
     def open(filepath):
-        pass
+        load(filename=filepath)
 
     # Does `info()` return a string with the name of the base model
     # and the timestamp of when it was initialized?
     def info(self):
         return f'{self.name}, initialized {self.timestamp}'
 
+
 if __name__ == '__main__':
-    machine = Machine(DataFrame({'Rarity':[0], 'Health':[0]}))
+    machine = Machine(DataFrame({'Rarity': [0], 'Health': [0]}))
     print(machine.info())
